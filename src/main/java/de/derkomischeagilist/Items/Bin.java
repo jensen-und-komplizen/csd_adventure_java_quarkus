65201f768bc07569702609b0bdd9f8fcd19b3f25
Robin Mobers <mobers@sipgate.de>
2023-11-28 16:16:24 +0100

    feat: Change text

--- src/main/java/de/derkomischeagilist/Items/Bin.java
+++ src/main/java/de/derkomischeagilist/Items/Bin.java
@@ -3,7 +3,7 @@ package de.derkomischeagilist.Items;
 public class Bin {
 
     public String getDescription() {
-        return "You walk closer to the bin. It is very dirty and smells like a wet dog. Inside you see a lot of used paper towels. No one would reach in here.";
+        return "You walk closer to the bin. It is very dirty and smells like a old wet chicken. Inside you see a lot of used paper towels. No one would reach in here.";
     }
 
 }
