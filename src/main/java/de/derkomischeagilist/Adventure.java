fdfe80c36ce37ee2f0033f952606a888d86896ab
MaLipski <Marcus.Lipski@cimt-ag.de>
2024-12-04 16:48:04 +0100

    Added new joke

--- src/main/java/de/derkomischeagilist/Adventure.java
+++ src/main/java/de/derkomischeagilist/Adventure.java
@@ -15,7 +15,7 @@ public class Adventure {
     private Room kitchen;
     private String lastResponse;
 
-    private String[] jokes = new String[] {"Why do we tell actors to 'break a leg?' - Because every play has a cast ;)", "Chuck Norris wins at Planning Poker."};
+    private String[] jokes = new String[] {"Why do we tell actors to 'break a leg?' - Because every play has a cast ;)", "Chuck Norris wins at Planning Poker.", "Was liegt am Strand und ist schwer zu verstehen? Eine Nuschel!"};
     int jokeCounter = 0;
 
     public Adventure() {
