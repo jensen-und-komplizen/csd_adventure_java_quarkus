1401177eae27532348bb9ad3c34e31ecc72c8d6c
MaLipski <Marcus.Lipski@cimt-ag.de>
2024-12-04 16:26:28 +0100

    EnteringAnInvalidCommandResultsInHelpfulResponse: ContainsStringIgnoringCase wurde ersetzt durch ContainsString. Der Text "You wake up on the loo" wurde angepasst.

--- src/test/java/de/derkomischeagilist/AdventureTest.java
+++ src/test/java/de/derkomischeagilist/AdventureTest.java
@@ -42,7 +42,7 @@ public class AdventureTest {
     @Test
     void LookingAroundInTheLooThenICanSeeMagazines() {
         //given i am on the loo
-        assertThat(adventure.Begin(), containsStringIgnoringCase("you wake up on the Loo"));
+        assertThat(adventure.Begin(), containsStringIgnoringCase("You wake up on the loo"));
         //When i look around
         String actual = adventure.tell("look around");
         //Then i can see magazines
@@ -94,7 +94,7 @@ public class AdventureTest {
     @Test
     void EnteringAnInvalidCommandResultsInHelpfulResponse() {
         //given i am on the loo
-        assertThat(adventure.Begin(), containsStringIgnoringCase("you wake up on the Loo"));
+        assertThat(adventure.Begin(), containsString("You wake up on the loo"));
         //When i look around
         String actual = adventure.tell("Do something stupid");
         //Then i can see magazines
